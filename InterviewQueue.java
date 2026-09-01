42e34c30efbd2f82bcee92512edaf27c7ce100be
2025sarrakhadse <2025.sarra.khadse@ves.ac.in>
2026-09-01 12:27:49 +0530

    Update InterviewQueue.java

--- InterviewQueue.java
+++ InterviewQueue.java
@@ -1,6 +1,6 @@
 public class InterviewQueue {
 
-    // Node represents one element of the queue
+    // Node of the queue
     private class Node {
 
         Application application;
@@ -12,23 +12,18 @@ public class InterviewQueue {
         }
     }
 
-    // Front = first application in the queue
+    // Front and rear
     private Node front;
-
-    // Rear = last application in the queue
     private Node rear;
-
-    // Number of applications in the queue
-    private int size;
 
     // Constructor
     public InterviewQueue() {
+
         front = null;
         rear = null;
-        size = 0;
     }
 
-    // Add application to the rear
+    // Add application to queue
     public boolean enqueue(Application application) {
 
         if (application == null) {
@@ -37,77 +32,80 @@ public class InterviewQueue {
 
         Node newNode = new Node(application);
 
-        // If queue is empty
         if (rear == null) {
 
             front = newNode;
             rear = newNode;
-            size++;
 
             return true;
         }
 
-        // Connect new node after rear
         rear.next = newNode;
-
-        // Move rear to the new node
         rear = newNode;
-
-        size++;
 
         return true;
     }
 
-    // Remove application from the front
+    // Remove application from front
     public Application dequeue() {
 
         if (front == null) {
-            System.out.println("Interview queue is empty.");
+
+            System.out.println(
+                    "Interview queue is empty."
+            );
+
             return null;
         }
 
-        // Store application being removed
         Application application = front.application;
 
-        // Move front to next node
         front = front.next;
 
-        // If queue becomes empty
         if (front == null) {
             rear = null;
         }
 
-        size--;
-
         return application;
     }
 
-    // View the next application without removing it
+    // View next interview
     public Application peek() {
 
         if (front == null) {
-            System.out.println("Interview queue is empty.");
+
+            System.out.println(
+                    "Interview queue is empty."
+            );
+
             return null;
         }
 
         return front.application;
     }
 
-    // Display all applications in queue
+    // Display queue
     public void displayQueue() {
 
         if (front == null) {
-            System.out.println("Interview queue is empty.");
+
+            System.out.println(
+                    "Interview queue is empty."
+            );
+
             return;
         }
 
-        Node current = front;
+        System.out.println(
+                "\n===== Interview Queue ====="
+        );
 
-        System.out.println("\n===== Interview Queue =====");
+        Node current = front;
 
         while (current != null) {
 
-            current.application.displayApplicationDetails();
+            current.application
+                    .displayApplicationDetails();
 
             current = current.next;
         }
@@ -119,17 +117,18 @@ public class InterviewQueue {
         return front == null;
     }
 
-    // Get number of applications
+    // Count interviews
     public int size() {
 
-        return size;
-    }
+        int count = 0;
+        Node current = front;
 
-    // Clear the entire queue
-    public void clear() {
+        while (current != null) {
 
-        front = null;
-        rear = null;
-        size = 0;
+            count++;
+            current = current.next;
+        }
+
+        return count;
     }
 }
