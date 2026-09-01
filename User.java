ff01c0663a6b89eefe8bab9ec08795c7acd46502
2025sarrakhadse <2025.sarra.khadse@ves.ac.in>
2026-09-01 12:02:07 +0530

    Add password support to User class

--- User.java
+++ User.java
@@ -4,12 +4,14 @@ public abstract class User {
     private int userId;
     private String name;
     private String email;
+    private String password;
 
     // Constructor
-    public User(int userId, String name, String email) {
+    public User(int userId, String name, String email, String password) {
         this.userId = userId;
         this.name = name;
         this.email = email;
+        this.password = password;
     }
 
     // Getter for userId
@@ -27,6 +29,11 @@ public abstract class User {
         return email;
     }
 
+    // Getter for password
+    public String getPassword() {
+        return password;
+    }
+
     // Setter for name
     public void setName(String name) {
         this.name = name;
@@ -35,6 +42,11 @@ public abstract class User {
     // Setter for email
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    // Setter for password
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     // Abstract method
